61dba761b6ceac774b637471182a12b3fe4fa8f0
ahmedssamy11555 <ahmedssamy11555@gmail.com>
2023-09-27 16:49:10 +0300

    function that prints _putchar

---
+++ 0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int function_that_prints_putcahr();
+int main(void){
+    function_that_prints_putcahr();
+    return 0;
+}
+
+int function_that_prints_putcahr(){
+    printf("_putchar\n");
+    return 0;
+}
