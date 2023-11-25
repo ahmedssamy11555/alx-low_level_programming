c63babdbe8243d7f91c17607fe9aa8ffea40391a
ahmedssamy11555 <ahmedssamy11555@gmail.com>
2023-11-25 16:24:04 +0200

    isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,22 @@
+/**
+* _isdigit - checks for uppercase character.
+*
+* Description: **checks for uppercase character.**
+*@c : int parameter
+* Return:
+* 1 if c is a digit
+* 0 otherwise
+*/
+
+int _isdigit(int c)
+{
+if ((char)c >= 48 && (char)c <= 57)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+return (0);
+}
