2080d6a586e5cbb1373ed02d41cba17d73e172cc
ahmedssamy11555 <ahmedssamy11555@gmail.com>
2023-12-23 12:31:43 +0200

    new changes+

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-gcc -c *.c files_c/*.c
-ar rc libmy.a *.o
-ranlib libmy.a                                  
+gcc -c *.c
+ar rc liball.a *.o
+ranlib liball.a                                  
